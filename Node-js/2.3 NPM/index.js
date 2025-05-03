d4873175141cd74254b71913182491fa564d4e5e
Aryan Sharma <86112346+aryan8180@users.noreply.github.com>
2025-05-03 19:48:37 +0000

    Created a qr-code project

---
+++ Node-js/2.3 NPM/index.js
@@ -0,0 +1,12 @@
+// var generateName = require('sillyname');
+
+// import generateName from "sillyname";
+
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}`)
+
+import superheroes from 'superheroes';
+
+const name = superheroes.random();  // ✅ should work
+console.log(`My name is ${name}!`);
